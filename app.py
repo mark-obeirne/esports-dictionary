279cdc85aa5df3d3a2a989e3625ba85d03222696
mark-obeirne <markobeirne00@gmail.com>
2020-09-10 12:33:16 +0000

    Connect to MongoDB and set up basic route
    Import PyMongo and ObjectId modules to handle connectivity to
    MongoDB.
    Set up function that connects to database and renders a template.

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import os
 from flask import (
     Flask, flash, render_template,
     redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
@@ -10,10 +12,14 @@ app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
+mongo = PyMongo(app)
+
 
 @app.route("/")
+@app.route("/show_terms")
 def test():
-    return "Setup correctly"
+    terms = mongo.db.terms.find()
+    return render_template("terms.html", terms=terms)
 
 
 if __name__ == "__main__":
