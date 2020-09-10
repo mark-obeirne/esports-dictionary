0afee7f5e74a0a01d5663d3a28d468f861e26a37
mark-obeirne <markobeirne00@gmail.com>
2020-09-10 20:15:20 +0000

    Add event listener to initialise select dropdown on terms.html
    Add event listener as per Materialize documentation to initialise the
    select dropdown featured on terms.html, which enables user to filter by
    game name

--- static/js/script.js
+++ static/js/script.js
@@ -3,3 +3,9 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, {edge: "right"});
   });
+
+// Initialize Dictionary Select Dropdown
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems, {});
+  }); 
