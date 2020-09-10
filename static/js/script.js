c00281f399256ae8973902b66d0987f1e397e1db
mark-obeirne <markobeirne00@gmail.com>
2020-09-10 13:24:57 +0000

    Initial commit
    Create file and initialise Materialize's sidenav trigger

---
+++ static/js/script.js
@@ -0,0 +1,5 @@
+// Initialize Sidenav
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, {edge: "right"});
+  });
