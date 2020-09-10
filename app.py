aa1529158ead83d05f088633b1d263fa60d6236b
mark-obeirne <markobeirne00@gmail.com>
2020-09-10 10:58:36 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,22 @@
+import os
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+if os.path.exists("env.py"):
+    import env
+
+app = Flask(__name__)
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+
+@app.route("/")
+def test():
+    return "Setup correctly"
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
