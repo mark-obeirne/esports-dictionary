7999052d2801b5126e6495b0426c8a9856f8f1a8
mark-obeirne <markobeirne00@gmail.com>
2020-10-02 07:13:06 +0000

    Set debug to False

--- app.py
+++ app.py
@@ -701,5 +701,5 @@ def invalid_route(e):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
     profanity.load_censor_words()
